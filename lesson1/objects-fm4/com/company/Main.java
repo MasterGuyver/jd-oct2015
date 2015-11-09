82fdec51da7e6a7bc6c3d910e97d4c31606d5246
MasterGuyver <masterguyver@gmail.com>
2015-11-10 00:19:16 +0200

    First Lesson

---
+++ lesson1/objects-fm4/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Tank tank = new Tank();
+        tank.color="Dark";
+        tank.crew = 6;
+        tank.maxSpeed = 110;
+        tank.printTankInfo();
+    }
+}
---
+++ lesson1/objects-fm4/com/company/Tank.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/**
+ * Created by Guyver on 09.11.15.
+ */
+public class Tank {
+    String color;
+    int crew;
+    int maxSpeed;
+    void printTankInfo() {
+        System.out.println("Tank Characteristics.");
+        System.out.println("Color: "+color);
+        System.out.println("Color: "+crew);
+        System.out.println("Color: "+maxSpeed);
+    }
+}
