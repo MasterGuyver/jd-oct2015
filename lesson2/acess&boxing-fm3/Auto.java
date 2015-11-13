9ce20774f7b7210b68b5dcf48bbfbe1e1528ee29
MasterGuyver <masterguyver@gmail.com>
2015-11-13 23:06:45 +0200

    Second lesson

---
+++ lesson2/acess&boxing-fm3/Auto.java
@@ -0,0 +1,14 @@
+
+public class Auto {
+
+	public static void main(String[] args) {
+		Integer a = 0;
+		int b = 6;
+		System.out.println("Sum is: "+revet(b,a));
+
+	}
+	static public int revet(Integer b, int a) {
+		int c = b + a;
+		return c;
+	}
+}
---
+++ lesson2/acess&boxing-fm3/NonAuto.java
@@ -0,0 +1,15 @@
+
+public class NonAuto {
+
+		public static void main(String[] args) {
+			Integer a = new Integer(0);
+			int b = 6;
+			System.out.println("Sum is: "+revet(a,b));
+
+		}
+		static public int revet(Integer b, int a) {
+			int c = b.intValue() + a;
+			return c;
+		}
+
+	}
